df325b6e30e0170cf96052663b7d4d338df66196
Henry Trinh <henry.trinh98@gmail.com>
2019-04-07 17:21:01 +1000

    henry

--- app/src/main/java/com/example/juzza/mygamifieddegree/HomeActivity.java
+++ app/src/main/java/com/example/juzza/mygamifieddegree/HomeActivity.java
@@ -10,6 +10,8 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        //leslie test
+        //leslie
+
+        /henry
     }
 }
