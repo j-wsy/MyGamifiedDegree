d5424422c55ba62189002c20d8ef497c5f2c32a6
lingkang shangguan <sg.brave@gmail.com>
2019-04-08 19:07:15 +1000

    garry's fragments commit

--- app/src/main/java/com/example/juzza/mygamifieddegree/MessageFragment.java
+++ app/src/main/java/com/example/juzza/mygamifieddegree/MessageFragment.java
@@ -1,5 +1,6 @@
 package com.example.juzza.mygamifieddegree;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,8 +9,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MessageFragment extends Fragment {
+
+
+  // mainpage
+
+
 
     @Nullable
     @Override
@@ -19,9 +26,43 @@ public class MessageFragment extends Fragment {
 
         return inflater.inflate(R.layout.fragment_message,container,false);
 
+
+
+
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        view.findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(),"123123",Toast.LENGTH_LONG).show();
+                Intent level3 = new Intent(getActivity(), level3.class);
+                startActivity(level3);
+
+            }
+        });
+
+        view.findViewById(R.id.button6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent level3 = new Intent(getActivity(), level3.class);
+                startActivity(level3);
+
+            }
+        });
+
+        view.findViewById(R.id.button7).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent level3 = new Intent(getActivity(), level3.class);
+                startActivity(level3);
+
+            }
+        });
+    }
 }
 
 
