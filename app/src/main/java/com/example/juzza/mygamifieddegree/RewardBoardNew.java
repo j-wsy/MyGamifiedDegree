7dccbc1c9cdec032d42b7e1e2b8df3cabe64ea61
yaowang <ericyaowang@gmail.com>
2019-04-08 18:18:27 +1000

    Eric commit 2

---
+++ app/src/main/java/com/example/juzza/mygamifieddegree/RewardBoardNew.java
@@ -0,0 +1,36 @@
+package com.example.juzza.mygamifieddegree;
+
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class RewardBoardNew extends AppCompatActivity {
+
+    ImageView newbie;
+    ImageView javaist;
+    ImageView javaExplorer;
+    ImageView javaStar;
+    ImageView javaGuru;
+    ImageView groupie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reward_board_new);
+
+        newbie = findViewById(R.id.badge1);
+        javaist = findViewById(R.id.badge2);
+        javaExplorer = findViewById(R.id.badge3);
+        javaStar = findViewById(R.id.badge4);
+        javaGuru = findViewById(R.id.badge5);
+        groupie = findViewById(R.id.badge6);
+
+
+
+
+
+
+    }
+}
