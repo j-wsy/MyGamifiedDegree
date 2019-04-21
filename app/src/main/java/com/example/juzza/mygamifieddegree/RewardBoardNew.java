8631103ef3478f19b59ab78d1ff20fc36b7aec61
yaowang <ericyaowang@gmail.com>
2019-04-21 18:32:16 +1000

    Garry --> Lock

--- app/src/main/java/com/example/juzza/mygamifieddegree/RewardBoardNew.java
+++ app/src/main/java/com/example/juzza/mygamifieddegree/RewardBoardNew.java
@@ -18,12 +18,122 @@ import android.widget.Toast;
 
 public class RewardBoardNew extends AppCompatActivity {
 
+    ImageView newbie;
+    ImageView Term1Yr1;
+    ImageView Term2Yr1;
+    ImageView Year1;
+    ImageView Javaist;
+    ImageView Jstar;
+    ImageView Jxplorer;
+    ImageView Jguro;
+    ImageView Grouple;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reward_board_new);
+
+
+        DbHelper dbHelper=new DbHelper(getApplicationContext());
+        newbie=findViewById(R.id.badge1);
+        Term1Yr1=findViewById(R.id.badge2);
+        Term2Yr1=findViewById(R.id.badge3);
+        Year1=findViewById(R.id.badge4);
+        Javaist=findViewById(R.id.badge5);
+        Jstar=findViewById(R.id.badge6);
+        Jxplorer=findViewById(R.id.badge7);
+        Jguro=findViewById(R.id.badge8);
+        Grouple=findViewById(R.id.badge9);
+
+
+        newbie.setImageResource(R.drawable.lock);
+        Term1Yr1.setImageResource(R.drawable.lock);
+        Term2Yr1.setImageResource(R.drawable.lock);
+        Year1.setImageResource(R.drawable.lock);
+        Javaist.setImageResource(R.drawable.lock);
+        Jstar.setImageResource(R.drawable.lock);
+        Jxplorer.setImageResource(R.drawable.lock);
+        Jguro.setImageResource(R.drawable.lock);
+        Grouple.setImageResource(R.drawable.lock);
+
+
+         int b=0;
+         int c=0;
+         c= 24 -(dbHelper.getRemainingCoreCourses()+dbHelper.getRemainingElectiveCourses()+dbHelper.getRemainingGeneralCourses());
+
+        if(c>=1) {
+           // newbie.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.colorYellow));
+            //newbie.setColorFilter(getApplicationContext().getResources().getColor(R.color.lightGrey));
+            newbie.setImageResource(R.drawable.newbie);
+        }
+
+
+
+        if(c>=3) {
+           // Term1Yr1.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.colorYellow));
+
+            //Term1Yr1.setColorFilter(getApplicationContext().getResources().getColor(R.color.lightGrey));
+            Term1Yr1.setImageResource(R.drawable.t1y1);
+        }
+
+
+        if(c>=6) {
+           // Term2Yr1.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.colorYellow));
+           // Term2Yr1.setColorFilter(getApplicationContext().getResources().getColor(R.color.lightGrey));
+
+
+            Term2Yr1.setImageResource(R.drawable.t2y1);
+        }
+
+
+        if(c>=9) {
+            //Year1.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.colorYellow));
+
+            Year1.setImageResource(R.drawable.t3y1);
+            //Year1.setColorFilter(getApplicationContext().getResources().getColor(R.color.lightGrey));
+        }
+
+         b=dbHelper.getIsCompleted("INFS1609");
+        if(b==1) {
+         //   Javaist.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.colorYellow));
+            //Javaist.setColorFilter(getApplicationContext().getResources().getColor(R.color.lightGrey));
+
+            Javaist.setImageResource(R.drawable.javaist);
+
+        }
+
+        b=dbHelper.getIsCompleted("INFS3830");
+        if(b==1) {
+            //Jstar.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.colorYellow));
+            //Jstar.setColorFilter(getApplicationContext().getResources().getColor(R.color.lightGrey));
+
+            Jstar.setImageResource(R.drawable.javasuperstar);
+        }
+
+        b=dbHelper.getIsCompleted("INFS2605");
+        if(b==1) {
+          //  Jxplorer.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.colorYellow));
+
+           Jxplorer.setImageResource(R.drawable.javaexplorer);
+            // Jxplorer.setColorFilter(getApplicationContext().getResources().getColor(R.color.lightGrey));
+        }
+
+        b=dbHelper.getIsCompleted("INFS3605");
+        if(b==1) {
+            //Jguro.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.colorYellow));
+
+            Jguro.setImageResource(R.drawable.javaguru);
+            // Jguro.setColorFilter(getApplicationContext().getResources().getColor(R.color.lightGrey));
+        }
+
+        b=dbHelper.getIsCompleted("INFS2608");
+        if(b==1) {
+           // Grouple.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.colorYellow));
+
+            Grouple.setImageResource(R.drawable.groupie);
+            //  Grouple.setColorFilter(getApplicationContext().getResources().getColor(R.color.lightGrey));
+        }
 
     }
 
