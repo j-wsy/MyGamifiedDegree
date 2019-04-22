86d1e12730407f621aae11a842d693917ce0c360
henry <henry.trinh98@gmail.com>
2019-04-22 16:03:56 +1000

    Changes to UI and added onboarding functionality - Jade

--- app/src/main/java/com/example/juzza/mygamifieddegree/MainActivity.java
+++ app/src/main/java/com/example/juzza/mygamifieddegree/MainActivity.java
@@ -22,9 +22,16 @@ public class MainActivity extends AppCompatActivity {
     EditText mTextUsername;
     EditText mTextPassword;
     Button mButtonLogin;
+    Button nextButton;
     TextView mTextViewRegister;
     DatabaseHelper db;
     Dialog dialog;
+    Dialog dialog2;
+    Dialog dialog3;
+    Dialog dialog4;
+    Dialog dialog5;
+    Dialog dialog6;
+
 
     ActionBar actionBar;
 
@@ -63,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
                     ImageView closeButton = (ImageView) dialog.findViewById(R.id.closeButton);
                     dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                     dialog.show();
+                    nextButton = (Button) dialog.findViewById(R.id.nextButton);
                     closeButton.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
@@ -72,6 +80,117 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
 
+                    nextButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog2 = new Dialog(MainActivity.this);
+                            dialog2.setContentView(R.layout.onboarding_1);
+                            ImageView closeButton = (ImageView) dialog2.findViewById(R.id.closeButton);
+                            dialog2.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                            dialog2.show();
+                            Button nextButton = (Button) dialog2.findViewById(R.id.nextButton);
+                            closeButton.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    dialog2.dismiss();
+                                    Intent LoginScreen = new Intent(MainActivity.this, HomeActivity.class);
+                                    startActivity(LoginScreen);
+                                }
+                            });
+
+                            nextButton.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    dialog3 = new Dialog(MainActivity.this);
+                                    dialog3.setContentView(R.layout.onboarding_2);
+                                    ImageView closeButton = (ImageView) dialog3.findViewById(R.id.closeButton);
+                                    dialog3.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                                    dialog3.show();
+                                    Button nextButton = (Button) dialog3.findViewById(R.id.nextButton);
+                                    closeButton.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            dialog3.dismiss();
+                                            Intent LoginScreen = new Intent(MainActivity.this, HomeActivity.class);
+                                            startActivity(LoginScreen);
+                                        }
+                                    });
+
+                                    nextButton.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            dialog4 = new Dialog(MainActivity.this);
+                                            dialog4.setContentView(R.layout.onboarding_3);
+                                            ImageView closeButton = (ImageView) dialog4.findViewById(R.id.closeButton);
+                                            dialog4.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                                            dialog4.show();
+                                            Button nextButton = (Button) dialog4.findViewById(R.id.nextButton);
+                                            closeButton.setOnClickListener(new View.OnClickListener() {
+                                                @Override
+                                                public void onClick(View v) {
+                                                    dialog4.dismiss();
+                                                    Intent LoginScreen = new Intent(MainActivity.this, HomeActivity.class);
+                                                    startActivity(LoginScreen);
+                                                }
+                                            });
+
+                                            nextButton.setOnClickListener(new View.OnClickListener() {
+                                                @Override
+                                                public void onClick(View v) {
+                                                    dialog5 = new Dialog(MainActivity.this);
+                                                    dialog5.setContentView(R.layout.onboarding_4);
+                                                    ImageView closeButton = (ImageView) dialog5.findViewById(R.id.closeButton);
+                                                    dialog5.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                                                    dialog5.show();
+                                                    Button nextButton = (Button) dialog5.findViewById(R.id.nextButton);
+                                                    closeButton.setOnClickListener(new View.OnClickListener() {
+                                                        @Override
+                                                        public void onClick(View v) {
+                                                            dialog5.dismiss();
+                                                            Intent LoginScreen = new Intent(MainActivity.this, HomeActivity.class);
+                                                            startActivity(LoginScreen);
+                                                        }
+                                                    });
+                                                    nextButton.setOnClickListener(new View.OnClickListener() {
+                                                        @Override
+                                                        public void onClick(View v) {
+                                                            dialog6 = new Dialog(MainActivity.this);
+                                                            dialog6.setContentView(R.layout.onboarding_5);
+                                                            ImageView closeButton = (ImageView) dialog6.findViewById(R.id.closeButton);
+                                                            dialog6.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                                                            dialog6.show();
+                                                            Button nextButton = (Button) dialog6.findViewById(R.id.nextButton);
+                                                            closeButton.setOnClickListener(new View.OnClickListener() {
+                                                                @Override
+                                                                public void onClick(View v) {
+                                                                    dialog6.dismiss();
+
+                                                                    Intent LoginScreen = new Intent(MainActivity.this, HomeActivity.class);
+                                                                    startActivity(LoginScreen);
+                                                                }
+                                                            });
+                                                            nextButton.setOnClickListener(new View.OnClickListener() {
+                                                                @Override
+                                                                public void onClick(View v) {
+                                                                    dialog6.dismiss();
+
+                                                                    Intent LoginScreen = new Intent(MainActivity.this, HomeActivity.class);
+                                                                    startActivity(LoginScreen);
+                                                                }
+                                                            });
+
+
+                                                        }
+                                                    });
+                                                }
+                                            });
+                                        }
+
+                                    });
+                                }
+                            });
+                        }
+                    });
                 } else {
                     Toast.makeText(MainActivity.this, "Error: Unable to log-in - could not find an existing user with that name and password!", Toast.LENGTH_SHORT).show();
                 }
